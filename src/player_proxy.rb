52ffd783bdfb7ea932b60caabe14942a1277beb4
Dustin Morrill <morrill@ualberta.ca>
2011-12-11 15:01:57 -0700

    Fixed a bunch of stuff and added a mixin.

--- src/player_proxy.rb
+++ src/player_proxy.rb
@@ -15,6 +15,9 @@ require 'acpc_poker_types'
 require 'acpc_poker_basic_proxy'
 require 'acpc_poker_match_state'
 
+# Local mixins
+require File.expand_path('../mixins/array_mixin', __FILE__)
+
 # A proxy player for the web poker application.
 class PlayerProxy
    include AcpcPokerTypesDefs
@@ -22,8 +25,7 @@ class PlayerProxy
    exceptions :match_ended
    
    # @return [Array<MatchState>] Summary of the progression of the match
-   #  in which, this player is participating since the last time +play!+ was called,
-   #  or the initial match states before this player was given a turn to act.
+   #  in which, this player is participating since this object's instantiation.
    attr_reader :match_snapshots
    
    # @param [DealerInformation] dealer_information Information about the dealer to which this bot should connect.
@@ -31,48 +33,75 @@ class PlayerProxy
    # @param [String] player_names The names of the players in this match.
    # @param [Integer] number_of_hands The number of hands in this match.
    def initialize(dealer_information, game_definition_file_name, player_names='user p2', number_of_hands=1)
+      game_definition = GameDefinition.new game_definition_file_name
       @basic_proxy = BasicProxy.new dealer_information
-      @match_state = MatchState.new(game_definition_file_name, next_match_state_string, player_names.split(/,?\s+/), number_of_hands)
-      @match_snapshots = [current_match_snapshot]
+      @match_snapshots = [MatchState.new(game_definition, next_match_state_string, player_names.split(/,?\s+/), number_of_hands)]
       
       update_match_state! unless users_turn_to_act?
    end
    
    # Player action interface
-   # @return [Array<MatchSnapshot>] Summary of the match's progression after taking the given +action+.
-   def play!(action, modifier=nil)
-      raise MatchEnded, "Cannot take action #{action} with modifier #{modifier} because the match has ended!" if match_ended?
+   # @param [PokerAction] action The action to take.
+   def play!(action)
+      raise MatchEnded, "Cannot take action #{action} because the match has ended!" if match_ended?
       
-      @basic_proxy.send_action action, modifier
+      @basic_proxy.send_action action
       
-      @match_snapshots = []
       update_match_state!
-      @match_snapshots
    end
-   
+      
    private
    
-   def current_match_snapshot
-      @match_state.dup
+   def current_match_state
+      @match_snapshots.last
    end
    
+   def take_match_snapshot
+      first_match_state = @match_snapshots.first
+      match_state = MatchState.new(first_match_state.game_definition,
+                                   first_match_state.match_state_string,
+                                   first_match_state.player_names,
+                                   first_match_state.number_of_hands)
+      
+      puts "   PLAYER_PROXY: current_match_snapshot!: first_match_state: #{first_match_state}, current_match_state: #{current_match_state}"
+      
+      @match_snapshots.rest.each do |previous_match_states|
+         match_state.update! previous_match_states.match_state_string
+      end
+      puts "   PLAYER_PROXY: current_match_snapshot!: @match_snapshots: #{@match_snapshots}"
+      puts "   PLAYER_PROXY: current_match_snapshot!: current_match_state: #{current_match_state}, match_state: #{match_state}"
+      
+      match_state
+   end
+   
+   
+   
    def update_match_state!
-      @match_state.update! next_match_state_string
-      @match_snapshots << current_match_snapshot
+      next_match_state = take_match_snapshot.update!(next_match_state_string)
+      
+      puts "   PLAYER_PROXY: update_match_state!: next_match_state: #{next_match_state}, @match_snapshots: #{@match_snapshots}"
+      
+      @match_snapshots << next_match_state
+      
+      puts "   PLAYER_PROXY: update_match_state!: @match_snapshots: #{@match_snapshots}"
+      
       update_match_state! unless (users_turn_to_act? or match_ended?)
    end
    
    def next_match_state_string
+      # @todo This BasicProxy method should have an ! at the end, since it changes its current_match_state
       @basic_proxy.receive_match_state_string
    end 
    
    # @see MatchState#users_turn_to_act?
-   def users_turn_to_act?      
-      @match_state.users_turn_to_act?
+   def users_turn_to_act?
+      puts "   PLAYER_PROXY: users_turn_to_act?: current_match_state: #{current_match_state}"
+      
+      current_match_state.users_turn_to_act?
    end
    
    # @see MatchState#match_ended?
    def match_ended?      
-      @match_state.match_ended?
+      current_match_state.match_ended?
    end
 end
